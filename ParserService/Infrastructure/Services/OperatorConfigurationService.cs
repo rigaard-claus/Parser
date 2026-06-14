c9b61378a80a1d156fa72d70aa909cc8b53d3410
Leonid <claus.leonid@gmail.com>
2026-06-14 19:04:51 +0300

    init parser

---
+++ ParserService/Infrastructure/Services/OperatorConfigurationService.cs
@@ -0,0 +1,39 @@
+﻿using ParserService.Data.Contexts;
+using ParserService.Data.Entities;
+using ParserService.ParserCore.Models;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+
+namespace ParserService.Infrastructure.Services
+{
+    public class OperatorConfigurationService(
+     DbTourParser tourParser,
+     IWebHostEnvironment env)
+    {
+        public async Task InitializeOperatorsAsync()
+        {
+            var filePath = Path.Combine(env.ContentRootPath, "ParserCore", "Config", "operators_config.json");
+            if (!File.Exists(filePath)) return;
+
+            var json = await File.ReadAllTextAsync(filePath);
+            var configs = JsonSerializer.Deserialize<List<TourOperator>>(json);
+
+            if (configs == null) return;
+
+            foreach (var config in configs)
+            {
+                if (!await tourParser.Operators.AnyAsync(o => o.Name == config.Name))
+                {
+                    var newOperator = new OperatorEntity
+                    {
+                        Name = config.Name,
+                        Priority = 1
+                    };
+
+                    tourParser.Operators.Add(newOperator);
+                }
+            }
+            await tourParser.SaveChangesAsync();
+        }
+    }
+}
---
+++ ParserService/ParserCore/Models/TourOperator.cs
@@ -0,0 +1,11 @@
+﻿namespace ParserService.ParserCore.Models
+{
+    public class TourOperator
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BaseUrl { get; set; }
+        public string ReferencesUrl { get; set; }
+        public string DataUrl { get; set; }
+    }
+}
